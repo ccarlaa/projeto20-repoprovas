35be34f930fe5a35d11dabeebaafc8b80ff630a9
Carla A. C. Ribeiro <carlacarlaclementino@gmail.com>
2022-07-25 16:38:15 -0300

    preparing for deploy: trying to fix export error

--- database.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var client_1 = require("@prisma/client");
-var PrismaClient = client_1["default"].PrismaClient;
-var prisma = new PrismaClient();
-exports["default"] = prisma;
